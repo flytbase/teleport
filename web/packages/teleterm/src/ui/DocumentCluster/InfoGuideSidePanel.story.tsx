8f625341a5b2f75f2176edb319e30699f2ea6dc7
Zac Bergquist <zac.bergquist@goteleport.com>
2026-04-21 15:15:54 -0700

    Fix stories and tests

--- web/packages/teleterm/src/ui/DocumentCluster/InfoGuideSidePanel.story.tsx
+++ web/packages/teleterm/src/ui/DocumentCluster/InfoGuideSidePanel.story.tsx
@@ -56,6 +56,7 @@ const rootClusterDoc = makeDocumentCluster({
 export function InfoGuideSidePanel() {
   const topBarConnectMyComputerRef = useRef<HTMLDivElement>(null);
   const topBarAccessRequestRef = useRef<HTMLDivElement>(null);
+  const desktopSessionControlsRef = useRef<HTMLDivElement>(null);
 
   const appContext = new MockAppContext();
   const cluster = makeRootCluster({
@@ -84,7 +85,10 @@ export function InfoGuideSidePanel() {
                         </StyledTabs>
                         <Example />
                       </Flex>
-                      <StatusBar onAssumedRolesClick={() => null} />
+                      <StatusBar
+                        onAssumedRolesClick={() => null}
+                        desktopSessionControlsRef={desktopSessionControlsRef}
+                      />
                     </Flex>
                   </InfoGuidePanelProvider>
                 </Wrapper>
